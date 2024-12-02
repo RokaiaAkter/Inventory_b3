6a202a5faef04d3beb370f135004b9610be21f1e
Lipi <Lipi@DESKTOP-6T25G03>
2024-12-02 13:06:59 +0100

    Add project files.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inventory_b3.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult Login()
+        {
+            return View();
+        }
+         //POST: Auth/Login
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+
+            if (username == "Admin" && password == "123")
+            {
+                Session["username"] = username;
+                ViewBag.Msg = "Login Success";
+                //Redirect to a secured page after successful login
+                //return RedirectToAction("index", "Home");
+            }
+            else
+            {
+                ViewBag.Msg = "Login Failed.";
+                //return View(); 
+            }
+            return View();
+
+           
+        }
+        // Action method for handling Logout
+       [ HttpPost]
+        public ActionResult Logout()
+        {
+            // Clear user session or cookies if needed
+            Session.Remove("username");
+            return RedirectToAction("Login");
+        }
+    }
+}
